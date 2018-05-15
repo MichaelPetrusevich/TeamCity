e26d1b239215c7071f4a8586d40779e6c296d051
MichaelPetrusevich <ip.petrusevich@gmail.com>
2018-05-15 11:51:18 +0300

    update_add_allure

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,21 +12,6 @@ exports.config = {
         browserName: 'chrome'
     },
 
-    onPrepare: function() {
-        var AllureReporter = require('jasmine-allure-reporter');
-        jasmine.getEnv().addReporter(new AllureReporter({
-            resultsDir: 'allure-results'
-        }));
-        jasmine.getEnv().afterEach(function(done){
-            browser.takeScreenshot().then(function (png) {
-                allure.createAttachment('Screenshot', function () {
-                    return new Buffer(png, 'base64')
-                }, 'image/png')();
-                done();
-            })
-        });
-    },
-
     // Spec patterns are relative to the configuration file location passed
     // to protractor (in this example config.js).
     // They may include glob patterns.
@@ -39,5 +24,24 @@ exports.config = {
         defaultTimeoutInterval: 30000
     },
 
+    onPrepare: function () {
+        var AllureReporter = require('jasmine-allure-reporter');
+
+        //Создание отчета
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+
+        //Создание скриншота
+        jasmine.getEnv().addReporter(new AllureReporter());
+        jasmine.getEnv().afterEach(function (done) {
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            })
+        });
+    }
 
 };
